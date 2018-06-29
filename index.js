b78c91e7a62f87fe1aca570851ee4ef2ad5dd1bb
zhoudejie <ssshooterx@gmail.com>
2018-06-29 16:49:45 +0800

    test

--- index.js
+++ index.js
@@ -1,38 +1,20 @@
-const puppeteer = require('puppeteer');
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 5000
+const MongoURI = process.env.MONGODB_URI
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('http://eshop-checker.xyz/beta/#/');
-  await page.waitForSelector('.item-detail-price')
-  await page.waitForSelector('.item-detail-region')
-  await page.waitForSelector('.square-img')
-  console.log('页面加载完成')
-  await page.screenshot({ path: 'example.png' });
-  let gameList = await page.$$('.item-container.item')
-  // console.log(gameList)
-  let price = gameList.map(gameNode => {
-    return gameNode.$eval('.item-visible-container', node => {
-      let img = node.getElementsByClassName('square-img')[0] // 当时无此div?
-      let isOnSale = node.getElementsByClassName('on-sale')[0] ? true : false
-      let title = node.getElementsByClassName('item-detail-title')[0].innerText
-      let release = node.getElementsByClassName('item-detail-release')[0].innerText
-      let region = node.getElementsByClassName('item-detail-region')[0].innerText
-      let price = node.getElementsByClassName('item-detail-price')[0].innerText
-      return {
-        img,
-        isOnSale,
-        title,
-        release,
-        region,
-        price
-      }
-    })
-  })
-  console.log('抽取游戏列表')
-  let priceList = await Promise.all(price)
-  console.log(priceList[0])
-  // console.log(priceList.map(val=>val.innerHTML))
+mongoose.connect(MongoURI)
 
-  await browser.close();
-})();
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {})
+var kittySchema = mongoose.Schema({
+  name: String
+})
+var Kitten = mongoose.model('Kitten', kittySchema)
+var felyne = new Kitten({ name: 'Felyne' })
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
